56cdbccb46c1c25f53af14b7f0ff95fb24934c61
k^2_08 <kirrud08@gmail.com>
2023-10-29 13:48:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
-from flask import Flask 
-from flask import Flask, render_template, request, redirect, url_for, flash
-app = Flask(__name__)         # Инициализация
-@app.route("/")               # Указание маршрута
-def home():
-    return render_template('index.html')
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash
 
-if __name__ == "__main__":
-    app.run(debug=True) 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+# Создание пользователя в базе данных
+@app.route('/create_user', methods=['POST'])
+def create_user(username,password):
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        return jsonify({'message': 'Пользователь с таким именем уже существует'})
+
+    new_user = User(username=username, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'Пользователь успешно создан'})
+
+create_user("SHKS","1234")
+
+
+# Вход пользователя
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json['username']
+    password = request.json['password']
+    user = User.query.filter_by(username=username).first()
+    if user and user.password == password:
+        # Здесь вы можете реализовать механизм входа для пользователя
+        return jsonify({'message': 'Вход успешно выполнен'})
+    return jsonify({'message': 'Неправильное имя пользователя или пароль'})
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
