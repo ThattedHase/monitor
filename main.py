6af1713ee331be62edd602d4cf447d1a5ebb7c31
k^2_08 <kirrud08@gmail.com>
2023-12-02 20:45:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,30 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import check_password_hash
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-
-# Создание пользователя в базе данных
-@app.route('/create_user', methods=['POST'])
-def create_user(username,password):
-    existing_user = User.query.filter_by(username=username).first()
-    if existing_user:
-        return jsonify({'message': 'Пользователь с таким именем уже существует'})
-
-    new_user = User(username=username, password=password)
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({'message': 'Пользователь успешно создан'})
-
-create_user("SHKS","1234")
+# Простой "база данных" для хранения пользователей (в реальном проекте используйте базу данных)
+users = {'иван': '1234','саня':'1234'}
 
 
-# Вход пользователя
-@app.route('/login', methods=['POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    username = request.json['username']
-    password = request.json['password']
-    user = User.query.filter_by(username=username).first()
-    if user and user.password == password:
-        # Здесь вы можете реализовать механизм входа для пользователя
-        return jsonify({'message': 'Вход успешно выполнен'})
-    return jsonify({'message': 'Неправильное имя пользователя или пароль'})
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Проверка наличия пользователя в "базе данных"
+        if username in users and users[username] == password:
+            # Перенаправление на главную страницу после успешной аутентификации
+            return redirect(url_for('main'))
+        else:
+            return render_template('login.html')
+
+    return render_template('login.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
